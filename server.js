89597c18f92bc7363a8c4b67c0346721b3411492
jakuboss <jakub16oss@gmail.com>
2020-04-27 12:25:51 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index') // routing do index.js
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //miejsce, gdzie przechowywane będą widoki
@@ -18,6 +21,10 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts) //odniesienie, ze bedziemy uzywać wzorca
 
 app.use(express.static('public')) //gdzie będziemy przechowywać pliki
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: false
+}))
 
 
 const mongoose = require('mongoose')
@@ -30,5 +37,6 @@ db.on('err', error => console.error(error)) //info w przypadku błędu
 db.once('open', () => console.log('Connected to Mongoose')) //połączenie z bazą danych
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000) //nasłuchiwanie na porcie 3000
