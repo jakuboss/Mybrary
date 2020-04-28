1547eeb3994a0dab9c2b9d078e5dbdf00aaf5f6f
jakuboss <jakub16oss@gmail.com>
2020-04-28 13:31:33 +0200

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -18,25 +18,23 @@ router.get('/', async (req, res) => {
         res.redirect('/')
     }
 })
-//New Author Route
-router.get('/new', async (req, res) => {
+// New Author Route
+router.get('/new', (req, res) => {
     res.render('authors/new', {
         author: new Author()
     })
-
 })
 
-
-
-//Create Author Route
+// Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
     try {
         const newAuthor = await author.save()
-        // res.redirect(`authors/${newAuthor.id`)
+        // res.redirect(`authors/${newAuthor.id}`)
         res.redirect(`authors`)
+
     } catch {
         res.render('authors/new', {
             author: author,
